921d4e3a4ee000f1190cb67e8266743aa0330dac
Martin Rykfors <martinrykfors@gmail.com>
2023-11-10 09:07:33 +0100

    Rename formatter `dotted` -> `gravel`

--- core/text/formatters.py
+++ core/text/formatters.py
@@ -142,7 +142,7 @@ formatters_words = {
     "all cap": formatters_dict["ALL_CAPS"],
     "no cap": formatters_dict["ALL_LOWERCASE"],
     "camel": formatters_dict["PRIVATE_CAMEL_CASE"],
-    "dotted": formatters_dict["DOT_SEPARATED"],
+    "gravel": formatters_dict["DOT_SEPARATED"],
     "dub string": formatters_dict["DOUBLE_QUOTED_STRING"],
     "dunder": formatters_dict["DOUBLE_UNDERSCORE"],
     "hammer": formatters_dict["PUBLIC_CAMEL_CASE"],
