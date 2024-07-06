34c0ef23cfd09a4ff2522ee7289a8bc40208b9b8
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-07-06 11:07:01 +0200

    Fix bug where you couldn't select help context by index (#1478)
    This broke in a recent update.

--- core/help/help.py
+++ core/help/help.py
@@ -710,7 +710,7 @@ class Actions:
                             (current_context_page - 1)
                             * settings.get("user.help_max_contexts_per_page")
                             + index
-                        ]
+                        ][0]
                     ]
 
     def help_previous():
