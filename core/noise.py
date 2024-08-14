cdffd9b6de24a2d260db8cdd215f57c3b7e4ccd6
Martin Rykfors <martinrykfors@gmail.com>
2024-08-14 13:00:23 +0200

    Made default noise implementations noop (#1500)

--- core/noise.py
+++ core/noise.py
@@ -15,6 +15,25 @@ class Actions:
         https://noise.talonvoice.com/static/previews/pop.mp3 for an
         example.
         """
+        actions.skip()
+
+    def noise_trigger_hiss(active: bool):
+        """
+        Called when the user makes a 'hiss' noise. Listen to
+        https://noise.talonvoice.com/static/previews/hiss.mp3 for an
+        example.
+        """
+        actions.skip()
+
+
+def noise_trigger_hiss_debounce(active: bool):
+    """Since the hiss noise triggers while you're talking we need to debounce it"""
+    global hiss_cron
+    if active:
+        hiss_cron = cron.after("100ms", lambda: actions.user.noise_trigger_hiss(active))
+    else:
+        cron.cancel(hiss_cron)
+        actions.user.noise_trigger_hiss(active)
 
 
 noise.register("pop", lambda _: actions.user.noise_trigger_pop())
